be6bc67bbcf81b6e5e27181b44042ba8ce0a34a2
Elizaveta <shes1996@bk.ru>
2022-07-29 15:20:57 +0300

    create task1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 1. Пользователь вводит число N (N > 0). Программа должна вывести N единиц на экран.
+N = 4 -> 1, 1, 1, 1
+N = 2 -> 1, 1 */
+
+void Zadacha1()
+{
+    int number = int.Parse(Console.ReadLine());
+    while (number>0)
+    {
+        Console.Write("1 ");
+        number--;
+    }
+}
+Zadacha1();
